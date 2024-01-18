ca3a57ff154ac477243cbd7ec5b273d5be86fb47
i-am-jiwon <dlark1128@gmail.com>
2024-01-18 22:00:55 +0900

    setting

---
+++ src/main/java/com/ll/sb240118/Sb240118Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb240118;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb240118Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb240118Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb240118/Sb240118ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb240118;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb240118ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
