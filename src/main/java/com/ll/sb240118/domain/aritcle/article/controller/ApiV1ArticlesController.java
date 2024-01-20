aeebbdc96384ddbd5ce95d4112fc8c1ba75c18ba
i-am-jiwon <dlark1128@gmail.com>
2024-01-21 00:04:35 +0900

    dto2

--- src/main/java/com/ll/sb240118/domain/aritcle/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/sb240118/domain/aritcle/article/controller/ApiV1ArticlesController.java
@@ -3,12 +3,14 @@ package com.ll.sb240118.domain.aritcle.article.controller;
 import com.ll.sb240118.domain.aritcle.article.dto.ArticleDto;
 import com.ll.sb240118.domain.aritcle.article.entity.Article;
 import com.ll.sb240118.domain.aritcle.article.service.ArticleService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -17,13 +19,24 @@ public class ApiV1ArticlesController {
 
  private final ArticleService articleService;
 
+ @Getter
+ public static class GetArticleResponseBody{
+     private final List<ArticleDto> items;
+     private final Map pagination;
+     public GetArticleResponseBody(List<Article> articles) {
+         items = articles
+                 .stream()
+                 .map(ArticleDto::new)
+                 .toList();
+
+         pagination = Map.of("page", 1);
+     }
+
+ }
+
     @GetMapping("")
-    public List<ArticleDto> getArticles(){
-        return articleService
-                .findAll()
-                .stream()
-                .map(article -> new ArticleDto(article))
-                .toList()
-                ;
+    public GetArticleResponseBody getArticles(){
+     return new GetArticleResponseBody(articleService.findAllByOrderByIdDesc());
     }
+
 }
--- src/main/java/com/ll/sb240118/domain/aritcle/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb240118/domain/aritcle/article/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package com.ll.sb240118.domain.aritcle.article.repository;
 import com.ll.sb240118.domain.aritcle.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByOrderByIdDesc();
 }
