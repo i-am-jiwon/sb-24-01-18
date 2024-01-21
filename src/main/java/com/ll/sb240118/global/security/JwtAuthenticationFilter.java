ac1b63ebce09fc53cee96b702e0f67b7abe9c058
i-am-jiwon <dlark1128@gmail.com>
2024-01-21 23:52:17 +0900

    dummy user

--- src/main/java/com/ll/sb240118/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ll/sb240118/global/security/JwtAuthenticationFilter.java
@@ -5,8 +5,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +22,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-        System.out.println("JwtAuthenticationFilter 실행");
+        User user = new User("user1", "", List.of());
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user,
+                user.getPassword(),
+                user.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
         filterChain.doFilter(request, response);
     }
